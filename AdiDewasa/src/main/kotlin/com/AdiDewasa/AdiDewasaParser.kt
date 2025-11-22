9521911d122fa889ce2a4160adbd5bbb964c1d9b
michat88 <gokilmichat@gmail.com>
2025-11-22 17:39:49 +0900

    Update AdiDewasaParser.kt

--- AdiDewasa/src/main/kotlin/com/AdiDewasa/AdiDewasaParser.kt
+++ AdiDewasa/src/main/kotlin/com/AdiDewasa/AdiDewasaParser.kt
@@ -2,40 +2,161 @@ package com.AdiDewasa
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class HomeResponse(
-    @JsonProperty("current_page") val currentPage: Int? = null,
-    @JsonProperty("data") val data: List<MediaItem>? = null,
-    @JsonProperty("first_page_url") val firstPageUrl: String? = null,
-    @JsonProperty("from") val from: Int? = null,
-    @JsonProperty("last_page") val lastPage: Int? = null,
-    @JsonProperty("last_page_url") val lastPageUrl: String? = null,
-    @JsonProperty("links") val links: List<Link>? = null,
-    @JsonProperty("next_page_url") val nextPageUrl: String? = null,
-    @JsonProperty("path") val path: String? = null,
-    @JsonProperty("per_page") val perPage: Int? = null,
-    @JsonProperty("prev_page_url") val prevPageUrl: String? = null,
-    @JsonProperty("to") val to: Int? = null,
-    @JsonProperty("total") val total: Int? = null,
+// --- Model Asli AdiDrakor ---
+data class AniIds(var id: Int? = null, var idMal: Int? = null)
+
+data class TmdbDate(val today: String, val nextWeek: String)
+
+data class VixsrcSource(val name: String, val url: String, val referer: String)
+
+data class VidrockSource(
+    @JsonProperty("resolution") val resolution: Int? = null,
+    @JsonProperty("url") val url: String? = null,
+)
+
+data class VidrockSubtitle(
+    @JsonProperty("label") val label: String? = null,
+    @JsonProperty("file") val file: String? = null,
+)
+
+data class VidsrccxSource(@JsonProperty("secureUrl") val secureUrl: String? = null)
+
+data class WyzieSubtitle(
+    @JsonProperty("display") val display: String? = null,
+    @JsonProperty("url") val url: String? = null,
+)
+
+data class VidFastSources(
+    @JsonProperty("url") val url: String? = null,
+    @JsonProperty("tracks") val tracks: ArrayList<Tracks>? = null,
+) {
+    data class Tracks(
+        @JsonProperty("file") val file: String? = null,
+        @JsonProperty("label") val label: String? = null,
+    )
+}
+
+data class VidFastServers(
+    @JsonProperty("name") val name: String? = null,
+    @JsonProperty("description") val description: String? = null,
+    @JsonProperty("data") val data: String? = null,
+)
+
+data class VidlinkSources(
+    @JsonProperty("stream") val stream: Stream? = null,
+) {
+    data class Stream(@JsonProperty("playlist") val playlist: String? = null)
+}
+
+data class MappleSubtitle(
+    @JsonProperty("display") val display: String? = null,
+    @JsonProperty("url") val url: String? = null,
+)
+
+data class MappleSources(
+    @JsonProperty("data") val data: Data? = null,
+) {
+    data class Data(@JsonProperty("stream_url") val stream_url: String? = null)
+}
+
+data class PrimeboxSources(
+    @JsonProperty("streams") val streams: HashMap<String, String>? = null,
+    @JsonProperty("subtitles") val subtitles: ArrayList<Subtitles>? = null,
+) {
+    data class Subtitles(
+        @JsonProperty("file") val file: String? = null,
+        @JsonProperty("label") val label: String? = null,
+    )
+}
+
+data class RageSources(@JsonProperty("url") val url: String? = null)
+
+data class VidsrcccServer(
+    @JsonProperty("name") val name: String? = null,
+    @JsonProperty("hash") val hash: String? = null,
+)
+
+data class VidsrcccResponse(@JsonProperty("data") val data: ArrayList<VidsrcccServer>? = arrayListOf())
+
+data class VidsrcccResult(@JsonProperty("data") val data: VidsrcccSources? = null)
+
+data class VidsrcccSources(
+    @JsonProperty("subtitles") val subtitles: ArrayList<VidsrcccSubtitles>? = arrayListOf(),
+    @JsonProperty("source") val source: String? = null,
+)
+
+data class VidsrcccSubtitles(
+    @JsonProperty("label") val label: String? = null,
+    @JsonProperty("file") val file: String? = null,
+)
+
+data class UpcloudSources(@JsonProperty("file") val file: String? = null)
+
+data class UpcloudResult(@JsonProperty("sources") val sources: ArrayList<UpcloudSources>? = arrayListOf())
+
+data class AniMedia(
+    @JsonProperty("id") var id: Int? = null,
+    @JsonProperty("idMal") var idMal: Int? = null
+)
+
+data class AniPage(@JsonProperty("media") var media: java.util.ArrayList<AniMedia> = arrayListOf())
+data class AniData(@JsonProperty("Page") var Page: AniPage? = AniPage())
+data class AniSearch(@JsonProperty("data") var data: AniData? = AniData())
+
+data class GpressSources(
+    @JsonProperty("src") val src: String,
+    @JsonProperty("file") val file: String? = null,
+    @JsonProperty("label") val label: Int? = null,
+    @JsonProperty("max") val max: String,
+)
+
+data class ResponseHash(
+    @JsonProperty("embed_url") val embed_url: String,
+    @JsonProperty("key") val key: String? = null,
+    @JsonProperty("type") val type: String? = null,
+)
+
+data class WatchsomuchTorrents(
+    @JsonProperty("id") val id: Int? = null,
+    @JsonProperty("movieId") val movieId: Int? = null,
+    @JsonProperty("season") val season: Int? = null,
+    @JsonProperty("episode") val episode: Int? = null,
+)
+
+data class WatchsomuchMovies(
+    @JsonProperty("torrents") val torrents: ArrayList<WatchsomuchTorrents>? = arrayListOf(),
+)
+
+data class WatchsomuchSubtitles(
+    @JsonProperty("url") val url: String? = null,
+    @JsonProperty("label") val label: String? = null,
+)
+
+data class WatchsomuchResponses(@JsonProperty("movie") val movie: WatchsomuchMovies? = null)
+data class WatchsomuchSubResponses(@JsonProperty("subtitles") val subtitles: ArrayList<WatchsomuchSubtitles>? = arrayListOf())
+
+data class IndexMedia(
+    @JsonProperty("id") val id: String? = null,
+    @JsonProperty("driveId") val driveId: String? = null,
+    @JsonProperty("mimeType") val mimeType: String? = null,
+    @JsonProperty("size") val size: String? = null,
+    @JsonProperty("name") val name: String? = null,
+    @JsonProperty("modifiedTime") val modifiedTime: String? = null,
+)
+
+data class IndexData(@JsonProperty("files") val files: ArrayList<IndexMedia>? = arrayListOf())
+data class IndexSearch(@JsonProperty("data") val data: IndexData? = null)
+
+// --- Model Khusus AdiDewasa ---
+data class AdiDewasaSearchResponse(
+    @JsonProperty("data") val data: ArrayList<AdiDewasaItem>? = arrayListOf(),
     @JsonProperty("success") val success: Boolean? = null
 )
 
-data class MediaItem(
-    @JsonProperty("is_adult") val isAdult: Int? = null,
+data class AdiDewasaItem(
     @JsonProperty("name") val name: String? = null,
     @JsonProperty("title") val title: String? = null,
     @JsonProperty("slug") val slug: String? = null,
     @JsonProperty("image") val image: String? = null,
-    @JsonProperty("poster") val poster: String? = null
-)
-
-data class Link(
-    @JsonProperty("url") val url: String? = null,
-    @JsonProperty("label") val label: String? = null,
-    @JsonProperty("active") val active: Boolean? = null
-)
-
-// Ganti nama class SearchResponse untuk menghindari konflik
-data class ApiSearchResponse(
-    @JsonProperty("data") val data: List<MediaItem>? = null,
-    @JsonProperty("success") val success: Boolean? = null
+    @JsonProperty("year") val year: String? = null
 )
