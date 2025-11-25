f5df6fb9ad0522980206c14546d2b47dc0475fa8
michat88 <gokilmichat@gmail.com>
2025-11-25 23:27:13 +0900

    Update AdiDewasaData.kt

--- AdiDewasa/src/main/kotlin/com/AdiDewasa/AdiDewasaData.kt
+++ AdiDewasa/src/main/kotlin/com/AdiDewasa/AdiDewasaData.kt
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class HomeResponse(
     @JsonProperty("data") val data: List<MediaItem>? = null,
     @JsonProperty("next_page_url") val nextPageUrl: String? = null,
-    @JsonProperty("success") val success: Boolean? = null // <-- PERBAIKAN: Field ini ditambahkan
+    @JsonProperty("success") val success: Boolean? = null
 )
 
 data class MediaItem(
@@ -23,16 +23,17 @@ data class ApiSearchResponse(
 )
 
 // --- Data Class Internal untuk Menyimpan Info Link ---
-
+// UPDATE: Menambahkan field tmdbId
 data class LinkData(
     val url: String,
     val title: String,
     val year: Int? = null,
     val season: Int? = null,
-    val episode: Int? = null
+    val episode: Int? = null,
+    val tmdbId: Int? = null 
 )
 
-// --- Helper Class untuk Extractor WpMovies & Primebox ---
+// --- Helper Class untuk Extractor ---
 
 data class ResponseHash(
     @JsonProperty("embed_url") val embed_url: String,
@@ -42,4 +43,17 @@ data class ResponseHash(
 
 data class PrimeboxSources(
     @JsonProperty("streams") val streams: Map<String, String>? = null
+)
+
+data class TmdbSearchResponse(
+    @JsonProperty("results") val results: List<TmdbResult>? = null
+)
+
+data class TmdbResult(
+    @JsonProperty("id") val id: Int? = null,
+    @JsonProperty("title") val title: String? = null,
+    @JsonProperty("release_date") val releaseDate: String? = null,
+    @JsonProperty("poster_path") val posterPath: String? = null,
+    @JsonProperty("overview") val overview: String? = null,
+    @JsonProperty("backdrop_path") val backdropPath: String? = null
 )
